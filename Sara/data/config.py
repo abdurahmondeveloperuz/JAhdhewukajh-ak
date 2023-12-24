2994b8074fe654bc25060f3b7ee96feeeacc73c5
abdurahmondeveloperuz <154701401+abdurahmondeveloperuz@users.noreply.github.com>
2023-12-24 08:44:53 +0500

    Add files via upload

---
+++ Sara/data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = "6357479039:AAEXJc0lK9lhi18TE1QiEALGz6Er9xIcPS0"  # Bot toekn
+ADMINS = ["6011359652","6466101131"]  # adminlar ro'yxati
+IP = "localhost" # Xosting ip manzili
